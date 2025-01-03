eb24035cc551619e90d82f169871266d29e9b6c2
Ryu Alvano <ryu.alvano@outlook.com>
2025-01-03 11:51:15 +0700

    learning components

--- lib/phoenixpractice_web/controllers/hello_controller.ex
+++ lib/phoenixpractice_web/controllers/hello_controller.ex
@@ -2,15 +2,25 @@ defmodule PhoenixpracticeWeb.HelloController do
   use PhoenixpracticeWeb, :controller
 
   def index(conn, _params) do
+    conn
+    |> put_flash(:info, "Welcome to Phoenix!")
+    |> render(:index)
+
     # render(conn, :index)
     # text(conn, "This is text example")
-    json(conn, %{
-      msg: "message",
-      sender: "Ryu Alvano"
-    })
+    # json(conn, %{
+    #   msg: "message",
+    #   sender: "Ryu Alvano"
+    # })
+    # redirect(conn, to: ~p"/hello/hai")
+    # redirect(conn, external: "https://docs.google.com/spreadsheets/d/1bWClj5LEiHSkgFpBwRTBRK4erNnuaf2jZkpnOHVlXbc/edit?gid=0#gid=0")
+    # send_resp(conn, 201, "This is success")
   end
 
   def show(conn, %{"msg" => msg}) do
-    render(conn, :show, msg: msg)
+    # render(conn, :show, msg: msg)
+    conn
+    |> assign(:msg, msg)
+    |> render(:show)
   end
 end
--- lib/phoenixpractice_web/controllers/hello_html.ex
+++ lib/phoenixpractice_web/controllers/hello_html.ex
@@ -2,4 +2,13 @@ defmodule PhoenixpracticeWeb.HelloHTML do
   use PhoenixpracticeWeb, :html
 
   embed_templates "hello_html/*"
+
+  attr :my_name, :string, default: "John"
+  def my_component(assigns) do
+    ~H"""
+    <h2>
+      Hello! This is my component and my name is {@my_name}
+    </h2>
+    """
+  end
 end
